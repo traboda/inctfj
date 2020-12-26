4879081995b2dc50c686cc5be0a79e42cf8c1ff2
Ashwin S Shenoy <aswinshenoy65@gmail.com>
2020-12-26 13:20:30 +0530

    Add Guest of Honour to Landing Page

--- src/components/landing/Schedule.jsx
+++ src/components/landing/Schedule.jsx
@@ -44,7 +44,42 @@ const Schedule = () => {
 
     return <div>
         <div className="container-lg px-3 py-5">
-            <h3 className="font-weight-bold">Schedule</h3>
+            <h3 className="font-weight-bold">Event Schedule</h3>
+            <div className="row mt-3 mb-4 px-1 mx-0">
+                <div className="col-md-4 d-flex align-items-center justify-content-center p-1 mb-3">
+                    <Fade left>
+                        <img
+                            alt="Dr. Biswajit Saha" draggable="false"
+                            style={{ width: '80%' }}
+                            src={require('../../assets/images/photos/guests/guest_of_honour.jpeg')}
+                        />
+                    </Fade>
+                </div>
+                <div className="col-md-8 d-flex align-items-center px-1">
+                    <Fade right>
+                        <div>
+                            <h5 className="font-weight-bold mb-0">Guest of Honour</h5>
+                            <h3 style={{ color: '#FF6F00' }} className="font-weight-bold mb-0">Dr. Biswajit Saha</h3>
+                            <div>Director, Central Board of Secondary Education</div>
+                            <div className="mt-2 " style={{ fontSize: '13px' }}>
+                                <p className="mb-1">
+                                    Dr. Biswajit Saha is Director (Trainings & Skill Education) in CBSE, New Delhi.
+                                    Manages the works of Skill curriculum design, development of Student resource books &
+                                    Training Manuals and Monitors the execution of various Teachers Training Programmes
+                                    by 16 CBSE-Centre of Excellences. He also represents CBSE in various National &
+                                    International Committees.
+                                </p>
+                                <p className="mb-0">
+                                    Dr Saha handled execution of various development projects of World Bank, AICTE, UGC and MeitY.
+                                    He had actively participated in the drafting of NVEQF, NSQF notified by MHRD & MSDE in 2012
+                                    & 2013 respectively. He drives promotion of future skill in schools with strong industry
+                                    association and oversee Digital Education movement under VidyaDaan @ DIKSHA.
+                                </p>
+                            </div>
+                        </div>
+                    </Fade>
+                </div>
+            </div>
             <h5 style={{ color: '#FF6F00' }}>Opening Ceremony</h5>
             <div className="row d-none d-md-flex font-weight-bold mx-0 py-2 border-bottom">
                 <div className="col-md-3  d-flex align-items-center p-1">
