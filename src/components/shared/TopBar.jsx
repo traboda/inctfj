679041e8144960a456f66705fa148a3330eb8d64
Advaith A J <advaitharunjeena@gmail.com>
2021-09-27 20:25:16 +0530

    Topbar dynamic visibility

--- src/components/shared/TopBar.jsx
+++ src/components/shared/TopBar.jsx
@@ -1,11 +1,11 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from "@emotion/styled";
 import { disableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
 import Fade from 'react-reveal/Fade';
 import Modal from "react-modal";
 
 import SideBar from "./SideBar";
-import {setUserInfo, useAuthState} from "../../states";
+import { setUserInfo, useAuthState } from "../../states";
 
 const TopbarContainer = styled.header`
   position: fixed;
@@ -13,68 +13,97 @@ const TopbarContainer = styled.header`
   left: 0;
   width: 100%;
   z-index: 6000;
-  box-shadow: 2px 3px 8px rgba(0, 0, 0, 0.35);
-  background: linear-gradient(to right, #263238, #212121);
-  border-bottom: 2px solid #FF6F00;
+  background: transparent;
+  border-bottom: 2px solid transparent;
+  box-shadow: none;
+  transition: all 250ms ease-in-out;
+  transform: translateY(-100%);
+  
+  &.floating {
+    background: white;
+    box-shadow: 2px 3px 8px rgba(0, 0, 0, 0.35);
+    border-bottom: 2px solid #FF6F00;
+  }
+  
+  &.up, &.top {
+    transform: none;
+  }
+
   nav {
-       a {
-          font-size: 17px;
-          color: #AAA;
-          padding: 8px 12px;
-          text-decoration: none!important;
-          &:hover {
-            color: #fd7e14;
-          }
-       }
+    a {
+      font-size: 17px;
+      color: #222;
+      padding: 8px 12px;
+      text-decoration: none !important;
+      transition: color 250ms ease;
+
+      &:hover {
+        color: #fd7e14;
+      }
     }
-  .logo{
+  }
+
+  .logo {
     max-height: 56px;
   }
+
   img {
     max-height: 32px;
     max-width: 100%;
   }
+
   button {
-      border: none;
-      padding: 0.5rem;
-      border-radius: 5px;
-      background: none;
-      &:hover, &:focus {
-        background: #E65100!important;
-        outline: none!important;
-      }
+    border: none;
+    padding: 0.5rem;
+    border-radius: 5px;
+    background: none;
+
+    &:hover, &:focus {
+      background: #E65100 !important;
+      outline: none !important;
+    }
   }
+
   padding: 0.35rem;
 `;
 
 
 const TopbarInfoCard = styled.div`
-    color: white;
-    line-height: 1;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    h5 {
-        font-size: 18px;
-        margin: 0;
-        color: #fd7e14;
-    }
-    button {
-      margin-left: 8px;
-      display: block;
-      background: #F13F17;
-      text-decoration: none!important;
-      color: white!important;
-      padding: 1rem;
-      font-weight: 600;
-      text-transform: uppercase;
-      border-radius: 10px;
-    }
+  color: #222;
+  line-height: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  h5 {
+    font-size: 18px;
+    margin: 0;
+    color: #fd7e14;
+  }
+
+  button {
+    margin-left: 8px;
+    display: block;
+    background: #F13F17;
+    text-decoration: none !important;
+    color: white !important;
+    padding: 1rem;
+    font-weight: 600;
+    text-transform: uppercase;
+    border-radius: 10px;
+  }
 `;
 
 const CloseButton = styled.button`
-   background: none!important; top: 1rem; right: 1rem; position: absolute; padding: 0!important;
-   img { width: 32px; }
+  background: none !important;
+  top: 1rem;
+  right: 1rem;
+  position: absolute;
+  padding: 0 !important;
+
+  img {
+    width: 32px;
+  }
 `;
 
 const TopBar = ({ darkenOnSidebar = false, UTMSource = null }) => {
@@ -83,10 +112,15 @@ const TopBar = ({ darkenOnSidebar = false, UTMSource = null }) => {
     const [showRegCard, setShowRegCard] = useState(false);
     const [hasLoaded, setLoaded] = useState(false);
     const [isLoggedIn] = useAuthState('isLoggedIn');
+    const [scrollDir, setScrollDir] = useState('up');
+    const [isAtTop, setIsAtTop] = useState(true);
 
-    useEffect(() => { setLoaded(true); }, []);
+    useEffect(() => {
+        setLoaded(true);
+    }, []);
 
     const topbarRef = useRef()
+    const scrollPrevStateRef = useRef(0);
 
     const onLogOut = () => {
         setUserInfo(null);
@@ -102,28 +136,33 @@ const TopBar = ({ darkenOnSidebar = false, UTMSource = null }) => {
         setShowMenu(false);
         clearAllBodyScrollLocks();
     };
-    //
-    // const [scroll, setScroll] = useState(0)
-    // const scrollListener = (e) => {
-    //     const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    //     const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    //
-    //     const scrolled = winScroll / height;
-    //     setScroll(scrolled);
-    // }
-    //
-    // useEffect(() => {
-    //     window.addEventListener('scroll', scrollListener)
-    // }, [])
+
+    const onScroll = () => {
+        let st = window.pageYOffset || document.documentElement.scrollTop;
+        st = st <= 0 ? 0 : st;
+
+        setScrollDir(st > topbarRef.current.scrollHeight && st > scrollPrevStateRef.current ? 'down' : 'up');
+        setIsAtTop(st <= topbarRef.current.scrollHeight);
+        scrollPrevStateRef.current = st;
+    }
+
+    useEffect(() => {
+        document.addEventListener('scroll', onScroll, false);
+        return () => {
+            document.removeEventListener('scroll', onScroll, false);
+        }
+    }, []);
 
     return <div>
-        <TopbarContainer ref={topbarRef}>
+        <TopbarContainer ref={topbarRef} className={scrollDir + ` ${isAtTop ? 'top' : 'floating'}`}>
             <div className="topbar-container">
                 <div className="container mx-auto sm:px-4 px-0">
                     <div className="flex flex-wrap  w-full mx-0">
-                        <div className="w-1/3 md:w-1/4 pr-4 pl-4 lg:w-1/5 pr-4 pl-4 md:text-center flex items-center md:justify-end justify-center px-2">
+                        <div
+                            className="w-1/3 md:w-1/4 pr-4 pl-4 lg:w-1/5 pr-4 pl-4 md:text-center flex items-center md:justify-end justify-center px-2">
                             <a href="/">
-                                <img className="logo" src={require('../../assets/images/logos/inctf_light.png')} alt="InCTFj" />
+                                <img className="logo" src={require('../../assets/images/logos/inctf.png')}
+                                     alt="InCTFj"/>
                             </a>
                         </div>
                         <div className="md:w-3/4 pr-4 pl-4 lg:w-4/5 pr-4 pl-4 px-1 hidden md:flex items-center">
@@ -154,7 +193,7 @@ const TopBar = ({ darkenOnSidebar = false, UTMSource = null }) => {
                         </div>
                         <div className="w-2/3 flex md:hidden items-center justify-end px-1">
                             <button onClick={onOpen}>
-                                <img alt="menu" src={require('../../assets/images/icons/hamburger.png')} />
+                                <img alt="menu" src={require('../../assets/images/icons/hamburger.png')}/>
                             </button>
                         </div>
                     </div>
@@ -163,7 +202,10 @@ const TopBar = ({ darkenOnSidebar = false, UTMSource = null }) => {
         </TopbarContainer>
         <Modal
             isOpen={showRegCard}
-            onRequestClose={() => { clearAllBodyScrollLocks(); setShowRegCard(false); }}
+            onRequestClose={() => {
+                clearAllBodyScrollLocks();
+                setShowRegCard(false);
+            }}
             style={{
                 overlay: {
                     zIndex: 9000, background: 'rgba(0,0,0,0.8)',
@@ -172,15 +214,18 @@ const TopBar = ({ darkenOnSidebar = false, UTMSource = null }) => {
                 },
                 content: {
                     position: 'unset', top: 0, left: 0, right: 0, padding: '15px',
-                    border: 'none', background: 'none',  width: '500px', maxWidth: '100vw'
+                    border: 'none', background: 'none', width: '500px', maxWidth: '100vw'
                 }
             }}
         >
             <CloseButton
                 className="px-4 border-0 rounded"
-                onClick={() => { clearAllBodyScrollLocks(); setShowRegCard(false)}}
+                onClick={() => {
+                    clearAllBodyScrollLocks();
+                    setShowRegCard(false)
+                }}
             >
-                <img alt="close" src={require('../../assets/images/icons/close.png')} />
+                <img alt="close" src={require('../../assets/images/icons/close.png')}/>
             </CloseButton>
             {showRegCard &&
             <div style={{ background: '#101219' }} className="flex p-2 items-center justify-center">
@@ -191,8 +236,9 @@ const TopBar = ({ darkenOnSidebar = false, UTMSource = null }) => {
                 />
             </div>}
         </Modal>
-        {showMenu && <SideBar darkenOnSidebar={darkenOnSidebar} onClose={onClose} isLoggedIn={hasLoaded && isLoggedIn} onLogOut={onLogOut} />}
-        <div style={{ height: topbarRef ? topbarRef?.current?.clientHeight : '72px'}} />
+        {showMenu && <SideBar darkenOnSidebar={darkenOnSidebar} onClose={onClose} isLoggedIn={hasLoaded && isLoggedIn}
+                              onLogOut={onLogOut}/>}
+        <div style={{ height: topbarRef ? topbarRef?.current?.clientHeight : '72px' }}/>
     </div>
 
 };
