0f3cce17de1f91ae3964ab71349c1e55d5301cdb
Ashwin S Shenoy <aswinshenoy65@gmail.com>
2021-10-09 18:42:01 +0530

    Add solving videos

--- src/components/resources/links.ts
+++ src/components/resources/links.ts
@@ -10,5 +10,14 @@ export default [
     },
     {
         url: 'https://www.youtube.com/embed/xpzb4S3G3FY?list=PLmpHrYT9y1LyQfWtPb3nX9bCgK_y8jr6U'
+    },
+    {
+        url: 'https://www.youtube.com/embed/i6z0laa85uc'
+    },
+    {
+        url: 'https://www.youtube.com/embed/SKtWfK_YrPE'
+    },
+    {
+        url: 'https://www.youtube.com/embed/sOSXP5AoOqQ'
     }
 ]
