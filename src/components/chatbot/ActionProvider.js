e5c5747575c8cd6da4d7bcee6c88e6b733e6c634
sevagenv <sevagenv@gmail.com>
2021-10-11 21:14:48 +0400

    feat: Add chatbot
    This commit add the files related to the proper working
    of the chatbot on the inctf site.
    
    Signed-off-by: sevagenv <sevagenv@gmail.com>

---
+++ src/components/chatbot/ActionProvider.js
@@ -0,0 +1,76 @@
+class ActionProvider {
+    constructor(
+     createChatBotMessage,
+     setStateFunc,
+     createClientMessage,
+     stateRef,
+     createCustomMessage,
+     ...rest
+   ) {
+     this.createChatBotMessage = createChatBotMessage;
+     this.setState = setStateFunc;
+     this.createClientMessage = createClientMessage;
+     this.stateRef = stateRef;
+     this.createCustomMessage = createCustomMessage;
+   }
+
+   updateChatbotState(botMessage) {
+    this.setState((prev) => ({
+      ...prev,
+      messages: [...prev.messages, botMessage],
+    }));
+   }
+
+   handleQuestions = (questions) => {
+    const message = this.createChatBotMessage("Check the following questions", {withAvatar: true, widget: "Questions"});
+
+    this.updateChatbotState(message);
+    this.setState((prev) => ({
+      ...prev,
+      questions: questions,
+    }))
+   }
+
+   handleInCTF = () => {
+     const messages = [
+       "CTFs (Abbreviation for Capture The Flag) are a type of computer security competition.",
+       "Contestants are presented with a set of challenges which test their creativity, technical (and googling) skills, and problem-solving ability."
+     ]
+
+     messages.forEach(message => 
+      (this.updateChatbotState(this.createChatBotMessage(message, {withAvatar: true,}))));
+   }
+
+   handlePlay = () => {
+     const message = this.createChatBotMessage("Unfortunately, yes. You will need to have a laptop or a PC in-order to participate in InCTF Junior.",
+     {
+       withAvatar: true,
+     })
+     this.updateChatbotState(message);
+   }
+
+   handleWin = () => {
+     const message = this.createChatBotMessage("Students finishing at top of the CTF leaderboard will be awarded with exciting goodies and prizes.", { withAvatar: true,})
+     this.updateChatbotState(message);
+   }
+
+   handleDefault = () => {
+      const message = this.createChatBotMessage("Sorry, I did not understand.", {withAvatar: true,});
+      this.updateChatbotState(message);
+   }
+
+   handleContact = () => {
+    const social = this.createChatBotMessage("You can contact us through the following means", 
+    {
+      widget: "Social"
+    })
+    this.updateChatbotState(social);
+   }
+
+   greet() {
+     const botMessage = this.createChatBotMessage('Hi friend');
+     this.updateChatbotState(botMessage);
+   }
+ }
+ 
+ export default ActionProvider;
--- src/data/faq.jsx
+++ src/data/faq.jsx
@@ -5,6 +5,9 @@ export default
     {
         "question": "Is this a quiz?",
         "keywords": ["quiz"],
+        "response": [
+            "No. InCTF is a Capture The Flag Championship, which is a more practical hands-on computer security competition."
+        ],
         "answer": <>
             No. InCTF is a Capture The Flag Championship, which is a more practical hands-on computer security competition.
             Unlike quizzes, a participant will be working with actual tools & using problem solving skills to solve
@@ -14,6 +17,10 @@ export default
     },
     {
         "question": "What is a Capture The Flag competition?",
+        "keywords": ["capture", "flag", "competition", "inctf"],
+        "response": [
+            "CTFs (Abbreviation for Capture The Flag) are a type of computer security competition. Contestants are presented with a set of challenges which test their creativity, technical (and googling)"
+        ],
         "answer": <>
             <p>
                 CTFs (Abbreviation for Capture The Flag) are a type of computer security competition.
@@ -30,6 +37,10 @@ export default
     },
     {
         "question": "Who can participate in Amrita InCTF Junior?",
+        "keywords": ["who", "participate"],
+        "response": [
+            "Each individual who participates in the competition in order to be eligible for the finals prizes must be: "
+        ],
         "answer": <>
             Each individual who participates in the competition in order to be eligible for
             the finals / prizes must be:
@@ -47,6 +58,10 @@ export default
     },
     {
         "question": "Do I need a laptop/PC to participate?",
+        "keywords": ["laptop", "pc", "participate"],
+        "response": [
+            "Unfortunately, yes. You will need to have a laptop or a PC in-order to participate in InCTF Junior."
+        ],
         "answer": <>
             Unfortunately, yes. You will need to have a laptop or a PC in-order to participate in InCTF Junior.
             You would need use techniques and some tools that only are available on a PC/Laptop.
@@ -55,6 +70,10 @@ export default
     },
     {
         "question": "How much does it cost to participate in Amrita InCTF Junior?",
+        "keywords": ["cost", "participate"],
+        "response": [
+            "Participation is fully free: there are no fees for participating in the contest."
+        ],
         "answer": <>
             Participation is fully free: there are no fees for participating in the contest.
         </>,
@@ -62,6 +81,10 @@ export default
     },
     {
         "question": "Why should I participate in this?",
+        "keywords": ["why", "participate"],
+        "response": [
+            "Developing skills in the area of cyber security opens up a whole new range of career opportunities for skilled graduates."
+        ],
         "answer": <>
             <p>
                 Developing skills in the area of cyber security opens up a whole new range of career opportunities for
@@ -79,6 +102,10 @@ export default
     },
     {
         "question": "Where does this happen?",
+        "keywords": ["venue", "where"],
+        "response": [
+            "The Internet! The competition will be indefinitely available online at this website."
+        ],
         "answer": <>
             The Internet! The competition will be indefinitely available online at this website. As long as you
             have the ability to install software and have unfiltered Internet access, the participants can compete
@@ -88,6 +115,10 @@ export default
     },
     {
         "question": "How can I prepare for this competition?",
+        "keywords": ["prepare", "preparation"],
+        "response": [
+            "The challenges from the previous round of InCTFj are accessible. The detailed solutions to these challenges will be put up later on."
+        ],
         "answer": <>
             <p>
                 The challenges from the previous round of InCTFj are accessible. The detailed solutions to these
@@ -102,6 +133,10 @@ export default
     },
     {
         "question": "What do I need for this competition?",
+        "keywords": ["need", "requirement"],
+        "response": [
+            "Full participation in the competition requires only -"
+        ],
         "answer": <>
             Full participation in the competition requires only -
             <ul className="mx-1">
@@ -115,6 +150,10 @@ export default
     },
     {
         "question": "What are the topics included?",
+        "keywords": ["topic", "topics"],
+        "response": [
+            "The following topics will be used for the challenges"
+        ],
         "answer": <>
             Our categories are
             <ol>
@@ -130,6 +169,10 @@ export default
     },
     {
         "question": "What do I get if I win?",
+        "keywords": ["win", "winning"],
+        "response": [
+            "Students finishing at top of the CTF leaderboard will be awarded with exciting goodies and prizes."
+        ],
         "answer": <>
             Students finishing at top of the CTF leaderboard will be awarded with exciting goodies and prizes,
             and they will also get access to exclusive online cyber-security training following the contest.
@@ -139,6 +182,10 @@ export default
     },
     {
         "question": "What constitutes as cheating?",
+        "keywords": ["cheating", "cheat"],
+        "response": [
+            "Attacking the scoring server, other teams, or machines not explicitly designated as targets is cheating."
+        ],
         "answer": <>
             <ul>
                 <li>
@@ -160,6 +207,10 @@ export default
     },
     {
         "question": "Are there any similar competitions?",
+        "keywords": ["similar", "competition"],
+        "response": [
+            "There are a few CTF’s that are intended to help high school students gain some experience"
+        ],
         "answer": <>
             There are a few CTF’s that are intended to help high school students gain some experience such as
             picoCTF, EasyCTF, HSCTF etc. You may try out the challenges from these CTF’s if available.
@@ -168,6 +219,10 @@ export default
     },
     {
         "question": "I have some doubts, where can I ask?",
+        "keywords": ["doubt", "doubts"],
+        "response": [
+            "You can contact us through our discord community or email us."
+        ],
         "answer": <>
             You can contact us through our discord community <a href="/discord" target="_blank">(Join)</a> for priority reply
             or email us at <a href="mailto:inctfj@am.amrita.edu">inctfj@am.amrita.edu</a>.
@@ -179,6 +234,9 @@ export default
         "keywords": [
             "team event", "participate as a team", "individually or teams",
         ],
+        "response": [
+            "No. InCTF Jr is an individual CTF competition exclusively for school students in India."
+        ],
         "answer": <>
             No. InCTF Jr is an individual CTF competition exclusively for school students in India.
         </>,
@@ -188,6 +246,9 @@ export default
         "question": "How many students from a school can qualify for the finals?",
         "keywords": [
             "students from a school", "my school"
+        ],
+        "response": [
+            "There is no restriction for the number of students who can participate from a school."
         ],
         "answer": <>
             There is no restriction for the number of students who can participate from a school.
@@ -201,6 +262,9 @@ export default
         "keywords": [
             "certificate of participation", "participation certificate"
         ],
+        "response": [
+            "Yes! You will get a digital participation certificate if you register and complete at-least 1 challenge for the qualifiers."
+        ],
         "answer": <>
             Yes! You will get a digital participation certificate if you register and complete
             at-least 1 challenge for the qualifiers. However, there is no participation certificate
@@ -211,6 +275,9 @@ export default
     {
         "question": "Can I participate if I an NRI student?",
         "keywords": ["nri"],
+        "response": [
+            "Yes! InCTF Jr is open for school students who are citizens of India including Non-Residents of India."
+        ],
         "answer": <>
             Yes! InCTF Jr is open for school students who are citizens of India including Non-Residents of India.
             However, overseas citizens of India, and students of Indian origin not holding an Indian citizenship
@@ -223,6 +290,10 @@ export default
         "questions": [
             "Should I be a school student to participate?",
             "Can only school students participate?"
+        ],
+        "keywords": ["school student", "highschool"],
+        "response": [
+            "InCTF Junior is a beginner-level cyber-security programme we organize exclusively for school students in India."
         ],
         "answer": <>
             <p>
@@ -239,6 +310,10 @@ export default
     },
     {
         "question": "Can I participate if I am not enrolled to any school ?",
+        "keywords": ["not enrolled"],
+        "response": [
+            "If you are a self-study student who have not enrolled in any traditional school under the age of 20,"
+        ],
         "answer": <>
             If you are a self-study student who have not enrolled in any traditional school under the age of 20,
             you can contact us (<a href="mailto:inctfj@am.amrita.edu">inctfj@am.amrita.edu</a>) to register
@@ -248,6 +323,10 @@ export default
     },
     {
         "question": "Is changing profile information possible after registration?",
+        "keywords": ["profile", "information"],
+        "response": [
+            "Until the qualifier CTF starts, participants are allowed to change some of their profile information such as name, school etc."
+        ],
         "answer": <>
             Until the qualifier CTF starts, participants are allowed to change some of their profile information
             such as name, school etc. You can perform this by going to <a href="https://app.traboda.com/profile/edit">app.traboda.com/profile/edit</a>.
@@ -258,6 +337,9 @@ export default
     {
         "question": "I forgot my password, what can I do?",
         "keywords": ['password', 'forgot'],
+        "response": [
+            "There is a password reset functionality available on Traboda."
+        ],
         "answer": <>
             <p>
                 There is a password reset functionality available on Traboda. You can go to the login page/login popup,
@@ -274,6 +356,9 @@ export default
     {
         "question": "What are the different types of Certificates I can get?",
         keywords: ["certificate", "types"],
+        "response": [
+            "A Participant may be eligible for the following types of certificates -"
+        ],
         "answer": <>
             <p>A Participant may be eligible for the following types of certificates -</p>
             <ul>
