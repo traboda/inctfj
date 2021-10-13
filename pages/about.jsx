4740aa33074519f51041fa9f66eb0a4ce7aa3128
Advaith A J <advaitharunjeena@gmail.com>
2021-10-13 21:57:24 +0530

    Achievements in about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -7,6 +7,7 @@ import PastINCTFSpeakers from "../src/components/about/PastSpeakers";
 import INCTFJStats from "../src/components/about/Stats";
 import InctfIntro from "../src/components/about/Intro";
 import Footer from "../src/components/shared/Footer";
+import AboutAchievements from "../src/components/about/Achievements";
 
 const AboutPageCoverSection = styled.div`
     background-image: ${() => `url(${require('../src/assets/images/covers/inctfj_mountains.jpg')})`};
@@ -22,6 +23,7 @@ const AboutPage = () => {
         <AboutPageCoverSection />
         <InctfIntro />
         <INCTFJStats />
+        <AboutAchievements />
         <PastINCTFSpeakers />
         <Footer />
     </Base>
