c9f690adb0aab38e1f53547b62edca24f4cf8510
Advaith A J <advaitharunjeena@gmail.com>
2021-10-14 15:54:13 +0530

    Writeup markdown viewer fix

--- pages/writeup/[id].jsx
+++ pages/writeup/[id].jsx
@@ -6,6 +6,108 @@ import Base from "../../src/components/shared/Base";
 import TopBar from "../../src/components/shared/TopBar";
 
 import Footer from "../../src/components/shared/Footer";
+import styled from "@emotion/styled";
+import dynamic from "next/dynamic";
+
+const CodeBlock = dynamic(() => import("../../src/components/CodeBlock"), { ssr: false });
+
+const Markdown = styled.div`
+  overflow-wrap: anywhere;
+  
+  p { margin-bottom: 1rem; }
+
+  a {
+    color: #F13F17;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  ol, ul {
+    list-style: revert;
+    margin: revert;
+    padding: revert;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    margin-bottom: 0.75rem !important;
+  }
+
+  h1, .modest-h1 {
+    font-size: 3rem;
+  }
+
+  h2, .modest-h2 {
+    font-size: 2.5rem;
+    margin-bottom: 15px;
+  }
+
+  h3, .modest-h3 {
+    font-size: 1.6rem;
+  }
+
+  h4, .modest-h4 {
+    font-size: 1.414rem;
+  }
+
+  h5, .modest-h5 {
+    font-size: 1.121rem;
+  }
+
+  h6, .modest-h6 {
+    font-size: .88rem;
+  }
+
+  p:last-child {
+    margin-bottom: 0;
+  }
+
+  pre {
+    width: 100%;
+    margin-bottom: 1rem;
+    border-radius: 7px;
+  }
+
+  p > code {
+    padding: 2px 6px;
+    background: rgb(206, 198, 198);
+    border-radius: 7px;
+  }
+
+  blockquote {
+    border-left: 8px solid #fafafa;
+    padding: 1rem;
+    font-style: italic;
+    background: rgb(75, 85, 99);
+    border-radius: 7px;
+    margin-bottom: 1rem;
+
+    & > blockquote {
+      margin-top: 1rem;
+    }
+  }
+
+  table {
+    width: 100%;
+    border-radius: 7px;
+    border-style: hidden;
+    text-align: left;
+    color: #333;
+    overflow: hidden;
+
+    thead th, thead td {
+      padding: 8px 12px;
+      background: #dedee6;
+    }
+
+    tbody th, tbody td {
+      border: 1px solid #dedee6;
+      padding: 8px 12px;
+      background: white;
+    }
+  }
+`;
 
 const WriteUpPage = ({ id }) => {
     const data = (() => {
@@ -40,9 +142,15 @@ const WriteUpPage = ({ id }) => {
                     </div>
                 </div>
                 <div className="py-4" style={{ minHeight: '70vh', maxWidth: '900px' }}>
-                    <div>
-                        <ReactMarkdown plugins={[remarkGfm]} children={data?.content} />
-                    </div>
+                    <Markdown className="leading-relaxed">
+                        <ReactMarkdown
+                            plugins={[remarkGfm]}
+                            children={data?.content}
+                            renderers={{
+                                code: CodeBlock
+                            }}
+                        />
+                    </Markdown>
                 </div>
             </div>
             <Footer />
