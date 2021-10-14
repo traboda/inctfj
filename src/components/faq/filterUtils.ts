0d1e8ca861d7cff911c3b8b3fac2ed26da199a1b
Advaith A J <advaitharunjeena@gmail.com>
2021-10-14 19:50:20 +0530

    Filter updated to include keywords

--- src/components/faq/filterUtils.ts
+++ src/components/faq/filterUtils.ts
@@ -4,7 +4,8 @@ const intersection = (set1, set2) => new Set([...set1].filter(x => set2.has(x)))
 
 export const filteredFAQ = (search, tags = new Set<any>(), totalVisible = null) => {
     let _faq = faq.filter(f => {
-        return f.question.toLowerCase().includes(search.toLowerCase()) &&
+        return (f.question.toLowerCase().includes(search.toLowerCase()) ||
+            f.keywords?.filter(keyword => keyword.includes(search))?.length) &&
             (tags.size > 0 ? intersection(f.tags, tags).size > 0 : true)
     });
     if (totalVisible) _faq = _faq.slice(0, totalVisible);
