eb54796a05bfabf730adfe55c6c2b8a348990d86
shogil <shogilbabu@gmail.com>
2021-10-29 15:08:20 +0530

    update the mail

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -96,7 +96,7 @@ const FAQPage = () => {
                     <div className="my-6 text-center">
                         <div style={{ color: '#111' }}>Did not find what you were looking for?</div>
                         <div style={{ color: '#222' }}>
-                            Write to us at <a href="mailto:inctfj@am.amrita.edu">inctfj@am.amrita.edu</a>
+                            Write to us at <a href="mailto:inctf@am.amrita.edu">inctfj@am.amrita.edu</a>
                         </div>
                     </div>
                 </Waypoint>
