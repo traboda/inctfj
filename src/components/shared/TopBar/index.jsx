cca44afc8fb75526c7feb0b6ff1f3e2b038d649a
shogil <shogilbabu@gmail.com>
2021-11-17 15:17:44 +0530

    update the login tab

--- src/components/shared/TopBar/index.jsx
+++ src/components/shared/TopBar/index.jsx
@@ -179,7 +179,7 @@ const TopBar = ({ darkenOnSidebar = false, UTMSource = null }) => {
                                         disableBodyScroll(document.body);
                                     }}
                                 >
-                                    Register <i className="fa fa-chevron-right"/>
+                                    Login <i className="fa fa-chevron-right"/>
                                 </button>
                             </TopbarInfoCard>
                         </div>
@@ -194,7 +194,7 @@ const TopBar = ({ darkenOnSidebar = false, UTMSource = null }) => {
                             }}
                             className="w-full px-5 py-4 font-semibold rounded-lg bg-primary text-white hover:bg-blue-800 shadow hover:shadow-xl ml-3"
                         >
-                            Register <i className="fa fa-chevron-right"/>
+                            Login <i className="fa fa-chevron-right"/>
                         </button>
                     </TopbarInfoCard>
                     <button onClick={onOpen} className="transition" style={{ width: 46, height: 46 }}>
