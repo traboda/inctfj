f00bb3d608f03d6a360265c7aafe9b4387a3e241
shogil <shogilbabu@gmail.com>
2021-11-22 12:02:00 +0530

    update the start page

--- pages/start.jsx
+++ pages/start.jsx
@@ -31,7 +31,7 @@ const StartPage = () => {
             <LandingStatsBar />
             <LandingPromoVideo />
             <LandingReasonsToParticipate />
-            <CTAReg UTMSource="start_page" />
+            {/*<CTAReg UTMSource="start_page" />*/}
             <LandingHowItWorks />
             {/*<LandingTestimonials />*/}
             <LandingSponsorship />
