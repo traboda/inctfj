5605b67a46cce64e235cc456609b5ed5f8e3bdda
shogil <shogilbabu@gmail.com>
2022-01-27 13:52:48 +0530

    update the winners page

--- pages/stats.jsx
+++ pages/stats.jsx
@@ -109,14 +109,14 @@ const Stats = () => {
 
                 {
                     "name": "SSOD",
-                    "school": "not specified",
+                    "school": "Company name not specified",
                     "title": "First prize Winners",
                     "members": "Ajay Suresh kumar, G S Nagendran, yuvaraj you_we(UV), Monish Kumar, Johan",
                     "avatar": require('../src/assets/images/champions/women.png')
                 },
                 {
                     "name": "ValueMentor",
-                    "school": "not specified",
+                    "school": "Company name not specified",
                     "title": "Second prize Winners",
                     "members": "Hemant Boban, Kishor B, Hariharan S, Rishikesh KS",
                     "avatar": require('../src/assets/images/champions/women.png')
