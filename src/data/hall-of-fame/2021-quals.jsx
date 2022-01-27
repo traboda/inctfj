e89181895e0b6f6012c1bc6cad31596317a947fa
shogil <shogilbabu@gmail.com>
2022-01-27 15:42:56 +0530

    update the winners page

--- src/data/hall-of-fame/2021-quals.jsx
+++ src/data/hall-of-fame/2021-quals.jsx
@@ -238,5 +238,309 @@ export default [
       "state": "",
       "points": 2301,
       "quota": "top30"
+    },
+    {
+      "rank": 31,
+      "username": "cryptonite",
+      "institution": "Manipal Institute of Technology",
+      "state": "IN-KA",
+      "points": 2201,
+      "quota": ""
+    },
+    {
+      "rank": 32,
+      "username": "shellpwn",
+      "institution": "Visvesvaraya National Institute of Technology, Nagpur",
+      "state": "IN-MH",
+      "points": 2001,
+      "quota": ""
+    },
+    {
+      "rank": 33,
+      "username": "opop",
+      "institution": "Indian Institute of Technology, Bombay (IIT-B)",
+      "state": "IN-MH",
+      "points": 1801,
+      "quota": ""
+    },
+    {
+      "rank": 34,
+      "username": "m1cr0endian",
+      "institution": "Indian Institute of Technology, Madras (IIT-M)",
+      "state": "IN-TN",
+      "points": 1701,
+      "quota": ""
+    },
+    {
+      "rank": 35,
+      "username": "pr0ce355_7",
+      "institution": "Amrita School of Engineering, Amritapuri",
+      "state": "IN-KL",
+      "points": 1501,
+      "quota": ""
+    },
+    {
+      "rank": 36,
+      "username": "kyros",
+      "institution": "PES University, Bengaluru",
+      "state": "IN-KA",
+      "points": 1401,
+      "quota": ""
+    },
+    {
+      "rank": 37,
+      "username": "underpants",
+      "institution": "S.A. Engineering college (SAEC), Chennai",
+      "state": "IN-TN",
+      "points": 1401,
+      "quota": ""
+    },
+    {
+      "rank": 38,
+      "username": "cli3nt",
+      "institution": "Cusrow Wadia Institute of Technology, Pune",
+      "state": "IN-MH",
+      "points": 1301,
+      "quota": ""
+    },
+    {
+      "rank": 39,
+      "username": "f.o.g.m.j",
+      "institution": "Manipal Institute of Technology",
+      "state": "IN-KA",
+      "points": 1201,
+      "quota": ""
+    },
+    {
+      "rank": 40,
+      "username": "randoms090",
+      "institution": "Lovely Professional University (LPU), Punjab",
+      "state": "IN-PB",
+      "points": 1201,
+      "quota": ""
+    },
+    {
+      "rank": 41,
+      "username": "0x00d3c0de",
+      "institution": "K J Somaiya College of Engineering (KJSCE), Mumbai",
+      "state": "IN-MH",
+      "points": 1001,
+      "quota": ""
+    },
+    {
+      "rank": 42,
+      "username": "___hk",
+      "institution": "Self Study",
+      "state": "",
+      "points": 1001,
+      "quota": ""
+    },
+    {
+      "rank": 43,
+      "username": "hackers253",
+      "institution": "National Institute of Technology (NIT), Meghalaya",
+      "state": "IN-ML",
+      "points": 1001,
+      "quota": ""
+    },
+    {
+      "rank": 44,
+      "username": "gamestonk",
+      "institution": "Indian Institute of Technology, Hyderabad (IIT-H)",
+      "state": "IN-TG",
+      "points": 601,
+      "quota": ""
+    },
+    {
+      "rank": 45,
+      "username": "akshipt",
+      "institution": "Acropolis Institute of Technology and Research, Indore",
+      "state": "IN-MP",
+      "points": 601,
+      "quota": ""
+    },
+    {
+      "rank": 46,
+      "username": "learners_",
+      "institution": "Narsee Monjee College of Commerce & Economics, Mumbai",
+      "state": "IN-MH",
+      "points": 601,
+      "quota": ""
+    },
+    {
+      "rank": 47,
+      "username": "stem_beacon",
+      "institution": "University of Madras",
+      "state": "IN-TN",
+      "points": 401,
+      "quota": ""
+    },
+    {
+      "rank": 48,
+      "username": "doofenshmirtz_evil_inc",
+      "institution": "Kalinga Institute of Industrial Technology,  Bhubaneswar",
+      "state": "IN-OR",
+      "points": 401,
+      "quota": ""
+    },
+    {
+      "rank": 49,
+      "username": "mritunjya",
+      "institution": "National Institute of Technology (NIT), Tiruchirapalli",
+      "state": "IN-TN",
+      "points": 301,
+      "quota": ""
+    },
+    {
+      "rank": 50,
+      "username": "4nonymous",
+      "institution": "Mukesh Patel School of Technology Management & Engineering (MPSTME), Mumbai",
+      "state": "IN-MH",
+      "points": 301,
+      "quota": ""
+    },
+    {
+      "rank": 51,
+      "username": "p4n1c",
+      "institution": "National Institute of Technology, Calicut",
+      "state": "IN-KL",
+      "points": 301,
+      "quota": ""
+    },
+    {
+      "rank": 52,
+      "username": "chaos",
+      "institution": "CMR College of Engineering and Technology, Hybedabad",
+      "state": "IN-TG",
+      "points": 301,
+      "quota": ""
+    },
+    {
+      "rank": 53,
+      "username": "pizza_dosa",
+      "institution": "National Institute of Technology (NIT), Tiruchirapalli",
+      "state": "IN-TN",
+      "points": 301,
+      "quota": ""
+    },
+    {
+      "rank": 54,
+      "username": "floppies",
+      "institution": "Gandhi Institute of Engineering and Technology, Gunupur",
+      "state": "",
+      "points": 301,
+      "quota": ""
+    },
+    {
+      "rank": 55,
+      "username": "jaisriram",
+      "institution": "Indian Institute of Technology, Hyderabad (IIT-H)",
+      "state": "IN-TG",
+      "points": 301,
+      "quota": ""
+    },
+    {
+      "rank": 56,
+      "username": "weimad",
+      "institution": "KL University, Guntur",
+      "state": "IN-AP",
+      "points": 201,
+      "quota": ""
+    },
+    {
+      "rank": 57,
+      "username": "7h35ud0",
+      "institution": "Chaitanya Bharathi Institute of Technology, Hyderabad",
+      "state": "IN-TG",
+      "points": 201,
+      "quota": ""
+    },
+    {
+      "rank": 58,
+      "username": "sup3rm4n",
+      "institution": "Lovely Professional University (LPU), Punjab",
+      "state": "IN-PB",
+      "points": 201,
+      "quota": ""
+    },
+    {
+      "rank": 59,
+      "username": "s4nd_hanitiz3r",
+      "institution": "NSS College of Engineering, Palakkad",
+      "state": "IN-KL",
+      "points": 201,
+      "quota": ""
+    },
+    {
+      "rank": 60,
+      "username": "kanyarashi",
+      "institution": "New Horizon College of Engineering, Bengaluru",
+      "state": "IN-KA",
+      "points": 101,
+      "quota": ""
+    },
+    {
+      "rank": 61,
+      "username": "crypto_risers",
+      "institution": "Amrita School of Engineering, Amritapuri",
+      "state": "IN-KL",
+      "points": 101,
+      "quota": ""
+    },
+    {
+      "rank": 62,
+      "username": "ledgers",
+      "institution": "Manipal Institute of Technology",
+      "state": "IN-KA",
+      "points": 101,
+      "quota": ""
+    },
+    {
+      "rank": 63,
+      "username": "bluemoon",
+      "institution": "Indira Gandhi Delhi Technical University For Women, Delhi",
+      "state": "IN-DL",
+      "points": 101,
+      "quota": ""
+    },
+    {
+      "rank": 64,
+      "username": "0b1ivi0n",
+      "institution": "Jalpaiguri Government Engineering College",
+      "state": "IN-WB",
+      "points": 101,
+      "quota": ""
+    },
+    {
+      "rank": 65,
+      "username": "unholy",
+      "institution": "Chandigarh University",
+      "state": "IN-CH",
+      "points": 1,
+      "quota": ""
+    },
+    {
+      "rank": 66,
+      "username": "h4xt3r",
+      "institution": "",
+      "state": "",
+      "points": 1,
+      "quota": ""
+    },
+    {
+      "rank": 67,
+      "username": "jnuites",
+      "institution": "Jawaharlal Nehru University (JNU), Delhi",
+      "state": "IN-DL",
+      "points": 1,
+      "quota": ""
+    },
+    {
+      "rank": 68,
+      "username": "power_rangers010",
+      "institution": "Banasthali Vidyapith, Vanasthali",
+      "state": "IN-RJ",
+      "points": 1,
+      "quota": ""
     }
   ]
