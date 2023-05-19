3cf94d04c3523e909e27abdd9fba080e13df579a
aswinshenoy <aswinshenoy65@gmail.com>
2023-05-19 14:51:35 +0530

    fixes

--- src/components/landing/About.tsx
+++ src/components/landing/About.tsx
@@ -18,7 +18,7 @@ const LandingAboutInCTF = () => (
               </span>
             </div>
             <div className="md:w-1/5 flex items-center md:my-0 my-3 md:px-0 justify-end">
-              <a href="/discord" className="bg-blue-800 mb-0 py-3">
+              <a href="/discord" className="bg-primary px-4 rounded-lg text-white mb-0 py-3">
                 Join Discord Now 
                 {' '}
                 <i className="fa fa-chevron-right ml-1" />
@@ -28,20 +28,20 @@ const LandingAboutInCTF = () => (
         </div>
       </div>
       <div className="flex flex-wrap mx-0">
-        <div className="md:w-2/3 p-2">
+        <div className="w-full md:w-2/3 p-2">
           <h2 className="mb-6 text-4xl">
             {data.LandingAboutInCTF[0]}
             <div className="text-primary">{data.LandingAboutInCTF[1]}</div>
             {data.LandingAboutInCTF[2]}
           </h2>
           {data.LandingDescINCTF.map((item, index) => (
-            <p className="mb-2 text-justify text-lg w-[720px] max-w-full" key={index}>
+            <p className="mb-2 text-justify text-base md:text-lg w-[720px] max-w-full" key={index}>
               {item}
             </p>
           ))}
         </div>
-        <div className="md:w-1/3 flex justify-start items-center">
-          <ul className="flex flex-col text-lg lg:text-xl gap-3">
+        <div className="md:w-1/3 flex justify-start items-center p-2">
+          <ul className="flex flex-col text-lg lg:text-xl font-semibold gap-3">
               {data.LandingHighlights.map((item, index) => (
                 <li key={index}>
                   {item}
