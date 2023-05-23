c0773cf06e1560dfb21ebefa9799b63df3590a95
Ashwin Shenoy <aswinshenoy65@gmail.com>
2023-05-23 23:38:55 +0530

    update domain

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -61,11 +61,11 @@ const LandingHeader = () => (
               </p>
             </div>
             <div className="-mx-2 mt-4">
-              <script src="https://inctfjr.ctf.eng.run/sdk.js" />
+              <script src="https://play.inctf.in/sdk.js" />
               <iframe
                   id="traboda-arena-reg-iframe"
                   className="w-full lg:h-[200px] h-[250px]"
-                  src="https://inctfjr.ctf.eng.run/reg-frame?background=%23fafafa&showLogin=true"
+                  src="https://play.inctf.in/reg-frame?background=%23fafafa&showLogin=true"
                   sandbox="allow-scripts allow-same-origin allow-top-navigation allow-forms allow-popups allow-pointer-lock allow-popups-to-escape-sandbox"
               />
             </div>
